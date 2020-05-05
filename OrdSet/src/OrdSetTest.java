60eb328ff596852a2b54c2d16320a525f159839f
Taina <tainaturella@gmail.com>
2020-05-04 23:52:07 -0300

    Criação da classe de testes e do esqueleto dos testes a serem implementados

---
+++ OrdSet/src/OrdSetTest.java
@@ -0,0 +1,28 @@
+import org.junit.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrdSetTest {
+
+    // Casos de teste elencados na questao 6
+    @Test
+    void a_avoidingOverflowConditionsTest(){
+    }
+    @Test
+    void b_withMaxElementQtyTest(){
+    }
+    @Test
+    void c_withMaxResizedTest(){
+    }
+    @Test
+    void d_withMaxResizedAndElementQtyTest(){
+    }
+
+    // Valores limite
+    @Test
+    void e_withValuesSmallerThanMinimal(){
+    }
+
+    // Cobertura de laços
+    // Term Coverage
+}
