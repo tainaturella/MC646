889966ed7e069aaefcea69bd2e05ba02b5821958
Taina <tainaturella@gmail.com>
2020-05-27 00:58:06 -0300

    Esqueleto dos casos de teste a serem implementados dado o que foi montado na tabela de combinações

---
+++ Elevator-source-code/ElevatorSystemTest.java
@@ -0,0 +1,24 @@
+import org.junit.Test;
+
+public class ElevatorSystemTest {
+    @Test
+    public void a_checkFloorTest(){}
+
+    @Test
+    public void b_doNothingTest(){}
+
+    @Test
+    public void c_stopAtFloorTest(){}
+
+    @Test
+    public void d_openDoorTest(){}
+
+    @Test
+    public void e_forceErrorTest(){}
+
+    @Test
+    public void f_closeDoorTest(){}
+
+    @Test
+    public void g_startMovingTest(){}
+}
